260bdc11bd56912468f7105bc0d0f79ad5a70305
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-11-30 11:15:24 -0800

    count the arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,10 @@ import sys
 argnum = len(sys.argv[1:])
 if argnum == 1:
     print(f"{argnum} argument:".format(sys.argv[1:]))
+    print(f"1: {sys.argv[1]}".format(sys.argv[1]))
+elif argnum == 0:
+    print(f"{argnum} arguments.".format(argnum))
 else:
-    print(f"
+    print(f"{argnum} arguments:".format(argnum))
+    for i in range(len(sys.argv[1:])):
+        print(f"{i + 1}: {sys.argv[i + 1]}".format(i + 1, sys.argv[i + 1]))
