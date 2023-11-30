060b0eabf54668ce806170616567d455c4f61c9f
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-11-30 11:17:19 -0800

    dont get greedy

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-argnum = len(sys.argv[1:])
-if argnum == 1:
-    print(f"{argnum} argument:".format(sys.argv[1:]))
-    print(f"1: {sys.argv[1]}".format(sys.argv[1]))
-elif argnum == 0:
-    print(f"{argnum} arguments.".format(argnum))
-else:
-    print(f"{argnum} arguments:".format(argnum))
-    for i in range(len(sys.argv[1:])):
-        print(f"{i + 1}: {sys.argv[i + 1]}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    argnum = len(sys.argv[1:])
+    if argnum == 1:
+        print(f"{argnum} argument:".format(sys.argv[1:]))
+        print(f"1: {sys.argv[1]}".format(sys.argv[1]))
+    elif argnum == 0:
+        print(f"{argnum} arguments.".format(argnum))
+    else:
+        print(f"{argnum} arguments:".format(argnum))
+        for i in range(len(sys.argv[1:])):
+            print(f"{i + 1}: {sys.argv[i + 1]}".format(i + 1, sys.argv[i + 1]))
