046688e353eef57ff7672cc3bca20341731984ef
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-12-04 03:40:00 -0800

    new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        new_list = my_list[:]
+    if idx < 0:
+        return new_list
+    if idx > (len(my_list) - 1):
+        return new_list
+    if element:
+        new_list[idx] = element
+        return new_list
