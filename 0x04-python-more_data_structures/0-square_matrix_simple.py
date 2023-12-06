cc196a00f992263d26e58d5cd0f8d95a331937ac
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-12-06 09:38:08 -0800

    squared simple fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square(x):
     return x * x
+
+
 def square_matrix_simple(matrix=[]):
-    matrix_cp = list(map(lambda row: list(map(square, row)),matrix))
+    matrix_cp = list(map(lambda row: list(map(square, row)), matrix))
     return matrix_cp
