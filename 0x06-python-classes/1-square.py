869450c79186656782bb351482c2fb9d6bad61d8
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-12-19 17:46:13 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""actual square module"""
+
+
+class Square:
+    """this class defines a square"""
+
+    def __init__(self, size):
+        """constructor
+
+        Args:
+            size: the size of the square
+        """
+
+        self.__size = size
