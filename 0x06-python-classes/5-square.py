8eba5049a1d9dc60c6fa412851eb30a98fcd5b5b
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-12-19 18:18:25 -0800

    print the sqaure

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""actual square module"""
+
+
+class Square:
+    """this class defines a square"""
+
+    def __init__(self, size=0):
+        """constructor
+
+        Args:
+            size: the size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """retriever
+
+        Returns:
+            the size attr
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """returns the area of the square
+
+        Returns:
+            the area
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ prints the square with #
+
+        Returns:
+            nothing
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print("")
