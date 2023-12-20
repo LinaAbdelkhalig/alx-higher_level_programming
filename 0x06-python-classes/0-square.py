d0060683596c475a0208c984d60380ea4a8c11b8
LinaAbdelkhalig <lelsammani0@gmail.com>
2023-12-19 17:40:25 -0800

    my first sqaure

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this is the Square module"""
+
+
+class Square:
+    """this is an empty class the defines a squre"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+this is the directory for the classes and objects project
