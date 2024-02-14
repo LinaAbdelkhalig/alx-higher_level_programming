c1f74221ad34b31ea60ec8072096a8bde325b8ef
Lena Abdelrazig <lelsammani0@gmail.com>
2024-02-14 23:37:59 +0200

    cities by states

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 --creates the table cities in the database above
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
