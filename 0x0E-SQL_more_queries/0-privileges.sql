625c4b9b830c1645ad1c1537d7c84c690dac9dee
Lena Abdelrazig <lelsammani0@gmail.com>
2024-02-14 22:27:22 +0200

    try this again

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists the privileges of the following users
-SHOW GRANT FOR 'user_0d_1'@'localhost'
-SHOW GRANT FOR 'user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
