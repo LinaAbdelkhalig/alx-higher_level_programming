d0f480a83f530b0c60ef450a32974fd6f203c191
LinaAbdelkhalig <lelsammani0@gmail.com>
2024-02-15 10:46:20 -0800

    fixes again

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
 -- convert the table first_table to utf-8
-ALTER TABLE first_table CONVERT TO CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table 
+CONVERT TO CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
