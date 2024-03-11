acb65079b7b5445b90e82b44e1bb49c0f0124c3d
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-03-11 16:12:02 -0700

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-function add(a, b)
-{
-	let args = process.argv;
-	a = parseInt(args[2]);
-	b = parseInt(args[3]);
-	let sum = a + b;
-	return sum;
+function add (a, b) {
+  const args = process.argv;
+  a = parseInt(args[2]);
+  b = parseInt(args[3]);
+  const sum = a + b;
+  return sum;
 }
 console.log(add());
