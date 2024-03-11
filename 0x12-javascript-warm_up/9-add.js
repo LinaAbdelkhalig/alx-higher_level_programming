8176019669be3470f66922cdf9cfe4437b3499de
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-03-11 16:10:35 -0700

    task 8

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 function add(a, b)
 {
+	let args = process.argv;
+	a = parseInt(args[2]);
+	b = parseInt(args[3]);
 	let sum = a + b;
-	console.log(sum);
+	return sum;
 }
+console.log(add());
