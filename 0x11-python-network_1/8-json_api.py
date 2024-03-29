74eea6c9ebf99ce2b0d11552fae9d8304532b120
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-03-28 17:44:47 -0700

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    this script takes a letter and sends a POST to
+    https://0.0.0.0:5000/search_user with the letter as the parameter
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        letter = sys.argv[1]
+    except IndexError:
+        letter = ""
+    
+    url = 'http://0.0.0.0:5000/search_user'
+    dat = {'q' : letter}
+
+    try:
+        r = requests.post(url, data=dat)
+        rjson = r.json()
+        if rjson:
+            print("[{}] {}".format(rjson.get('id'), rjson.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
